423c196b15e3843952a16f4a25af0a9f7db432bc
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2021-02-11 17:57:47 +0500

    [App Test] | change the test to ESS to find in the dom element

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Ehsan');
+  const linkElement = screen.getByText(/ESS/i);
   expect(linkElement).toBeInTheDocument();
 });
